b5759bf578b48447a10e13055dccea0f40a8811c
antoniolinhart <antonio.linhart@gmail.com>
2021-01-30 13:57:44 -0600

    Update App.js with Home and 4 space indent

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">Cue</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      </Navbar>
-
-      <p>
-        Test App
-      </p>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Navbar.Brand href="#home">Cue</Navbar.Brand>
+                <Nav.Link href="#home">Home</Nav.Link>
+            </Navbar>
+            <p>
+                Test App
+            </p>
+        </div>
+    );
 }
 
 export default App;
