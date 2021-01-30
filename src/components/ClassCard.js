c3459196adf64c4af334109db1fc882d6bc3ee21
antoniolinhart <antonio.linhart@gmail.com>
2021-01-30 17:13:36 -0600

    Update CSS for page with Nord

--- src/components/ClassCard.js
+++ src/components/ClassCard.js
@@ -4,15 +4,17 @@ import { Button, Card } from 'react-bootstrap';
 export const ClassCard = () => {
     return (
         <Card>
-            <Card.Header as="h5">Class Name</Card.Header>
             <Card.Body>
+                <h2>
+                    Course Name
+                </h2>
                 <Card.Text>
                     X Students in Queue
                 </Card.Text>
                 <Card.Text>
                     2 Active TAs
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button className="button-join">Join Queue</Button>
             </Card.Body>
         </Card>
     );
