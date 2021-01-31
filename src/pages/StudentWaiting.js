8f9a3fcad21d301fbf1e77ca1619b97df26842cd
antoniolinhart <antonio.linhart@gmail.com>
2021-01-30 23:25:32 -0600

    Framework for waiting page

--- src/pages/StudentWaiting.js
+++ src/pages/StudentWaiting.js
@@ -1,13 +1,26 @@
-import { Form } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 
 import { AuthUserContext } from '../components';
 
-const StudentWaiting = () => {
+const StudentWaiting = ({ studentsAhead, activeTas }) => {
     return (
         <AuthUserContext.Consumer>
             {(authUser) => !authUser ? null : (
                 <>
-                    <h1>Waiting page</h1>
+                    <h1>You're in the queue!</h1>
+                    <Row>
+                        <Col className="students-ahead" md={5}>
+                            <h1>5</h1>
+                            <p>students ahead of you</p>
+                        </Col>
+                        <Col md={7}>
+                            Active teaching assistants
+                            <ul>
+                                <li>Matty Vav</li>
+                                <li>Antonio Linhart</li>
+                            </ul>
+                        </Col>
+                    </Row>
                 </>
             )}
         </AuthUserContext.Consumer>
