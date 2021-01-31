66339d5fca8e04584dd012c76937739cc12f3c5e
Matt Vav <mathvav@users.noreply.github.com>
2021-01-30 22:32:04 -0600

    Minor navbar changes

--- src/components/ProjectNavbar/ProjectNavbar.js
+++ src/components/ProjectNavbar/ProjectNavbar.js
@@ -4,6 +4,8 @@ import { useLocation } from 'react-router-dom';
 import { withFirebase } from '../Firebase';
 import { AuthUserContext } from '../Session';
 
+import "./ProjectNavbar.css";
+
 const ProjectNavbar = ({ firebase, ...otherProps }) => {
     const location = useLocation();
 
@@ -11,7 +13,7 @@ const ProjectNavbar = ({ firebase, ...otherProps }) => {
         <Navbar className="nav-color" variant="dark" expand="lg">
             <Navbar.Brand href="/">Cue</Navbar.Brand>
             <Nav className="mr-auto">
-                <Nav.Link active={location.pathname === "/"} href="/">Home</Nav.Link>
+                <Nav.Link active={location.pathname === "/"} href="/">My Courses</Nav.Link>
             </Nav>
             <Nav>
                 <AuthUserContext.Consumer>
