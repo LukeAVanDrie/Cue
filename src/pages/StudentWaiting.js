313339961b22870675061c0ab3b39e9844eec283
antoniolinhart <antonio.linhart@gmail.com>
2021-01-31 00:44:06 -0600

    Student View

--- src/pages/StudentWaiting.js
+++ src/pages/StudentWaiting.js
@@ -1,4 +1,4 @@
-import { Col, Row } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 
 import { AuthUserContext } from '../components';
 
@@ -7,20 +7,23 @@ const StudentWaiting = ({ studentsAhead, activeTas }) => {
         <AuthUserContext.Consumer>
             {(authUser) => !authUser ? null : (
                 <>
-                    <h1>You're in the queue!</h1>
+                    <h1 className="text-center">You're in the queue!</h1>
                     <Row>
-                        <Col className="students-ahead" md={5}>
-                            <h1>5</h1>
+                        <Col className="students-ahead" md={6}>
+                            <span>5</span>
                             <p>students ahead of you</p>
                         </Col>
-                        <Col md={7}>
-                            Active teaching assistants
+                        <Col className="active-tas" md={6}>
+                            <span>Active teaching assistants</span>
                             <ul>
                                 <li>Matty Vav</li>
                                 <li>Antonio Linhart</li>
                             </ul>
                         </Col>
                     </Row>
+                    <div className="content-center">
+                        <Button variant="cue" className="remove mt-4" size="lg">I no longer need help</Button>
+                    </div>
                 </>
             )}
         </AuthUserContext.Consumer>
